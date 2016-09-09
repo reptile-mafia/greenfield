23076be784559d486001791798146fc8296ce651
Christopher Kiel <no-fi@users.noreply.github.com>
2016-09-09 12:38:34 -0500

    Adds VideoDescription component to App and connects it to onVideoChange callbacks

--- client/components/videoDetails.js
+++ client/components/videoDetails.js
@@ -2,38 +2,32 @@ import React from 'react';
 import { getDescription } from '../models/videoModel.js';
 
 export default class VideoDescription extends React.Component {
-	
+
 	constructor (props) {
 		super(props)
 		this.state = {
 			url : '',
 			title: ''
 		}
+	}
 
-		getDescription(props.url)
+	componentWillReceiveProps (nextProps) {
+		getDescription(nextProps.url)
 			.then( title => {
-				
 				this.setState({
 					title: title
 				});
-				
+				console.log('Title: ', title);
 			}).fail(err => console.log(err));
 	}
-
 
 	render(){
 		return(
 			<div>
 				<div className= "details">
 					<h2>{ this.state.title }</h2>
-					<div>Description</div>
 				</div>
 			</div>
-
-
-
-			);
-		}
+		);
+	}
 }
-
-
